76ec2c8244347c530c8f3a9e1a6b1cb1b3ec5f13
safwan <122959950+safwanaec@users.noreply.github.com>
2023-02-08 14:09:29 +0600

    Add files via upload

---
+++ class7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+    printf("Hello world!C\n");
+
+    printf("Hello world!C programming!\n\n\n");
+
+
+    int a = 2147483647; //max space is 32 bits
+
+    int _b_b = 5;
+
+    int c;
+
+    c = a + _b_b;
+
+    printf("The sum of %d and %d is: %d\n\n",a,_b_b,c);
+
+    /*while (a<=2147483649){
+
+        printf("%d\n",a);
+
+        ++a;
+
+
+    }*/
+
+
+    return 0;
+}
