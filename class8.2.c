d6e182463149c4b803b79bb17a55aefc7b793dee
safwan <122959950+safwanaec@users.noreply.github.com>
2023-02-09 16:35:53 +0600

    Add files via upload

---
+++ class8.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+float a;
+printf("Enter the value of Height : ");
+scanf("%f", &a);
+ int b;
+ printf("Enter the value of length  : ");
+scanf ("%d", &b);
+ float sum=.5*a*b;
+ printf("the area will be: %f",sum);
+ return 0;
+ }
---
+++ class8.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+
+
+int main(){
+
+short int x = 32767;
+
+int a,b,c;
+
+printf("\n\n\nvalue of x : %d\n\n\n",x);
+
+printf("\n welcome. this program adds\n");
+
+printf("three numbers. enter the three numbers\n");
+
+printf("in the form: nnn nnn nnn <return>\n\n\n");
+
+/*scanf("%d%d%d",&a,&b,&c);
+
+printf("Enter the value of a : ");
+
+scanf("%d",&a);
+
+printf("Enter the value of b : ");
+
+scanf("%d",&b);
+
+printf("Enter the value of c : ");
+
+scanf("%d",&c);
+
+//printf("\n\n\na is %d \nb is %d\nc is %d",a,b,c);
+
+int sum;
+
+sum = a+b+c;
+
+printf("\n\n\nThe total sum of %d, %d and %d is: %d",a,b,c,sum);*/
+
+printf("\n\n\n\n");
+
+
+/*float q;
+
+printf("Enter the percentage value: ");
+
+scanf("%f",&q);
+
+float w = q/100;
+
+int m,t;
+
+printf("\n\n");
+
+printf("Enter the amount of money: ");
+
+scanf("%d",&m);
+
+printf("\n\n");
+
+printf("Enter the amount of time in days: ");
+
+scanf("%d",&t);
+
+float T = t/365;
+
+float f = w*m*T;
+
+printf("\n\n");
+
+printf("The total profit will be: %5.2f Taka",f);
+
+printf("\n\n");*/
+
+
+
+
+
+
+
+
+return 0;
+}
