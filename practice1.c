890da7cd4d8f6f33b610749ace6124aac1fb68a5
safwan <122959950+safwanaec@users.noreply.github.com>
2023-02-28 16:25:30 +0600

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -1,35 +1,22 @@
 #include<stdio.h>
 
-int n1,n2,n3,a1;
+int main(void){
 
-void sc3num(void);
+int x = 5/2;
 
-int main()
-{
-    printf("This function takes 3 numbers as input and supposedly multiplys them.\n\n");
-    
-    sc3num();
-    
-    a1 = n1 * n2 * n3;
-    
-    printf("%d multiplied by %d and %d is equal to %d",n1,n2,n3,a1);
-    
-    printf("\n\nThis function takes 3 numbers as input and supposedly adds them.\n\n");
-    
-    sc3num();
-    
-    a1 = n1 + n2 + n3;
-    
-    printf("%d added with %d and %d is equal to %d",n1,n2,n3,a1);
-    
-    
-    return 0;
+float y = 5/2;
+
+printf("%f\n",x);
+
+printf("%d\n",x);
+
+printf("%f\n",y);
+
+printf("%d\n",x);
+
+// % is used for module division it gives the bhagsesh
+
+
+
+return 0;
 }
-
-void sc3num(void){
-
-    printf("Please input 3 numbers.\n");
-    
-    scanf("\n%d \n%d \n%d",&n1,&n2,&n3);
-    
-    } 
