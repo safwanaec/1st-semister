16e0286464cab08d31a4fa95037d14e5e925a8c9
safwan <122959950+safwanaec@users.noreply.github.com>
2023-03-11 01:41:48 +0600

    Add files via upload

---
+++ chapter6(6.1).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,m,o=0;
+
+    //take number from user
+    printf("Please enter a number: ");
+    scanf("%d",&n);
+
+    //use loop to module devide the number > multiply by 10 > module devide the remaining  number > add to first answer of module divition
+    while(n>9)
+    {
+          m=n%10;
+          m=o+m;
+          o=m*10;
+          n=n/10;
+
+    }
+    printf("The digit wise reverse number is: %d",o+n);
+
+    return 0;
+}
