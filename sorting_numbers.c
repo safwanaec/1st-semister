3c9e0b94dc0979082aa79bf756eb17eab4d26a39
safwan <122959950+safwanaec@users.noreply.github.com>
2023-03-16 15:21:38 +0600

    Update and rename sorting_numbers(incompleate).c to sorting_numbers.c

--- sorting_numbers.c
+++ sorting_numbers.c
@@ -4,45 +4,34 @@ int main(void)
     int n,temp;
     printf("How many integers do you want to enter?\n");
     scanf("%d",&n);
-    int a[n],b[n];
+    int a[n], b[n];
 
     printf("\n\nPlease enter %d integers to sort them: \n",n);
 
     for(int i=0; i<n; i++)
     {
         scanf("%d",&a[i]);
-        b[i]=a[i];
+        b[i]=0;
     }
 
     for(int i=0; i<n; i++)
     {
-        int k=0,l=0;
+        int max=0,x=0;
         for(int j=0; j<n; j++)
         {
-            if(a[i]>a[j])
+            if(max<=a[j])
             {
-                k++;
-            }
-            if(b[i]<b[j])
-            {
-                l++;
+                max=a[j];
+                x=j;
             }
         }
-        temp=a[k];
-        a[k]=a[i];
-        a[i]=temp;
-
-        temp=b[l];
-        b[l]=b[i];
-        b[i]=temp;
+        b[i]=max;
+        a[x]=0;
     }
-    printf("\n\n\n");
-
-    printf("smallest to largest\t\tlargest to smallest\n\n");
     for(int i=0; i<n; i++)
     {
-        printf("\t%d\t\t\t\t%d\n",a[i],b[i]);
-    }
+        printf("%d, ",b[i]);
 
+    }
     return 0;
 }
