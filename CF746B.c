b3f92afebdac6ab41b0d21756f2e5bfd6b5324bb
safwan <122959950+safwanaec@users.noreply.github.com>
2023-03-29 17:57:36 +0000

    commit

---
+++ CF746B.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    
+    printf("please enter the ammount of words you will write : ");
+    scanf("%d")
+
+    return 0;
+}
---
+++ litarals.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int a[10]={04463,0X543fe,0b01101101};
+float b[10]={314159E-5L};
+
+double litarals();
+
+int main(void)
+{
+    litarals();
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+double litarals()
+{
+     printf("\n%d\t%d\t%d\t%f\t\n",a[0],a[1],a[2],b[0]);
+     return 0;
+}
