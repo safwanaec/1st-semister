f7d3a4adf46e80d713a47d1cf041ac7fa76fe34c
safwan <122959950+safwanaec@users.noreply.github.com>
2023-03-31 23:55:36 +0600

    Update chapter6(6.2).c

--- chapter6(6.2).c
+++ chapter6(6.2).c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(void)
 {
-    int i,n,j,k=1;
+    int i,n,j;
+    unsigned long k=1;
     //take number from user
     printf("Please enter the value of n to get n! : ");
     scanf("%d",&n);
-
     //in a for loop take an integer i from 1 to the value of n
     //take an integer that has the initial value of 1 and multiply it by i
     //then assign the multiplied value to j itself
@@ -23,11 +23,11 @@ int main(void)
         k=k*i;
         if(k!=1)
         {
-            printf("= %d",k);
+            printf("= %lu",k);
         }
         printf("\n\n");
     }
     //print j
-    printf("\n\n%d! = %d\n\n",n,k);
+    printf("\n\n%d! = %lu\n\n",n,k);
     return 0;
 }
