40d1a1f3c4114eb401a19e123d7c948814a6c235
safwan <122959950+safwanaec@users.noreply.github.com>
2023-08-13 01:26:42 -0400

    Update 12-3-23.c

--- 12-3-23.c
+++ 12-3-23.c
@@ -1,7 +1,38 @@
-#include<stdio.h>
-int main(void)
-{
-    printf("yo brooooooooooooo\n\n");
+#include <stdio.h>
+
+void findSymmetricPairs(int arr[][2], int size) {
+    printf("Symmetric pairs:\n");
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i][0] == arr[j][1] && arr[i][1] == arr[j][0]) {
+                printf("(%d,%d) and (%d,%d) are symmetric pairs.\n", arr[i][0], arr[i][1], arr[j][0], arr[j][1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    printf("Enter the elements of the 2D array:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    findSymmetricPairs(arr, size);
 
     return 0;
 }
